911b9333a2e9d2cc421e7f9318bc7e142c5cfaf9
VIVEK M <vivekmprivate1908@gmail.com>
2026-07-27 14:01:09 +0530

    commit

---
+++ src/main/java/com/verify_x/services/HRDashboardService.java
@@ -0,0 +1,21 @@
+package com.verify_x.services;
+
+import com.verify_x.dto.HRDashboardResponseDto;
+import com.verify_x.entity.Candidate;
+
+import java.util.List;
+
+public interface HRDashboardService {
+
+    /**
+     * Returns the dashboard summary.
+     */
+    HRDashboardResponseDto getDashboardReport();
+
+    /**
+     * Returns all candidates for the
+     * Recent Applications table.
+     */
+    List<Candidate> getAllCandidates();
+
+}
